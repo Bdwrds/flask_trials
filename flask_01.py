abe1bd55549efe1091b7746e1498b8c96712b885
Benjamin Edwards <edwards.benjamin.james@gmail.com>
2019-01-12 11:37:33 +0000

    Initial basic demo flask app

---
+++ flask_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 11 19:58:58 2019
+
+@author: benjaminedwards
+"""
+
+
+from flask import Flask
+app_lala = Flask(__name__)
+
+@app_lala.route('/hello_page_lala')
+def hello_page_lulu():
+    return('Hello World!')
+
+if __name__=="__main__":
+    app_lala.run(debug=True)
+    
+    
